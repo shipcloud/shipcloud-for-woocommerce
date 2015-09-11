68b21f7bd56af202b277b16d60ca8313e5a81c80
Sven Wagener <sven.wagener@rheinschmiede.de>
2015-09-11 17:34:55 +0200

    Added buttons and changed Text

--- components/woo/order.php
+++ components/woo/order.php
@@ -380,11 +380,12 @@ class WC_Shipcloud_Order
 			<div class="parcel-template-field parcels-recommended">
 				<label for="parcels_recommended"><?php echo esc_attr( 'Parcels which have been calulated for Order', 'woocommerce-shipcloud' ); ?></label>
 				<select name="parcels_recommended">
-					<option value="none"><?php echo esc_attr( '[ Select a Parcel Template ]', 'woocommerce-shipcloud' ); ?></option>
+					<option value="none"><?php echo esc_attr( '[ Select a Parcel ]', 'woocommerce-shipcloud' ); ?></option>
 					<?php foreach( $parcel_templates AS $parcel_template ): ?>
 						<option value="<?php echo $parcel_template[ 'value' ];?>"><?php echo $parcel_template[ 'option' ];?></option>
 					<?php endforeach; ?>
 				</select>
+				<input type="button" name="add_parcel" value="<?php echo esc_attr( 'Insert in Form', 'woocommerce-shipcloud' ); ?>" class="button" />
 			</div>
 
 			<div class="parcel-template-field parcels-templates">
@@ -395,6 +396,7 @@ class WC_Shipcloud_Order
 					<option value="<?php echo $parcel_template[ 'value' ];?>"><?php echo $parcel_template[ 'option' ];?></option>
 					<?php endforeach; ?>
 				</select>
+				<input type="button" name="add_parcel" value="<?php echo esc_attr( 'Insert in Form', 'woocommerce-shipcloud' ); ?>" class="button" />
 			</div>
 
 		</div>
