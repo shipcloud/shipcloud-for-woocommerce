eaa0fa52c1c0bad3d3a938f246d79bb64b18d09e
Mike Pretzlaw <mail@mike-pretzlaw.de>
2017-08-30 07:31:28 +0200

    TDD: Test cash on delivery request

---
+++ tests/Shipcloud/api/v1/shipments/services/cash-on-delivery-test.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace WooCommerce_Shipcloud\Tests\Shipcloud\Api\v1\Shipments\Services;
+
+use Shipcloud\Domain\Services\CashOnDelivery;
+use WooCommerce_Shipcloud\Tests\Shipcloud\ShipcloudTestCase;
+
+class Cash_On_Delivery_TestCase extends ShipcloudTestCase {
+
+
+	protected function createShipmentsRequestData() {
+		return [
+			'from'                  => $this->createAddress()->toArray(),
+			'to'                    => $this->createAddress()->toArray(),
+			'package'               => $this->createPackage(),
+			// TODO Test multiple carrier instead of only one.
+			'carrier'               => 'dhl',
+			'create_shipping_label' => (bool) random_int( 0, 1 ),
+		];
+	}
+
+	/**
+	 *
+	 * @group acceptance
+	 * @group api
+	 */
+	public function testItCorrectlySendsDataToTheApi() {
+		$data     = $this->createShipmentsRequestData();
+
+		$data['additional_services'] = [
+			[
+				'name' => CashOnDelivery::NAME,
+				'properties' => $this->createCashOnDelivery()->toArray(),
+			]
+		];
+
+		$shipment = _wcsc_api()->shipment()->create( $data );
+
+		static::assertNotNull($shipment->getId());
+	}
+}
