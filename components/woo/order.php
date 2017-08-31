da992ba0a9916ee1c2d6651645f6f32d18722dd1
André Cedik <andre.cedik@gmail.com>
2017-08-31 13:58:14 +0200

    fallbacks for displaying street numbers

--- components/woo/order.php
+++ components/woo/order.php
@@ -780,7 +780,7 @@ class WC_Shipcloud_Order
 					<div class="label-shipment-sender address">
 						<div class="sender_company"><?php echo $data[ 'sender_company' ]; ?></div>
 						<div class="sender_name"><?php echo $data[ 'sender_first_name' ]; ?> <?php echo $data[ 'sender_last_name' ]; ?></div>
-						<div class="sender_street"><?php echo $data[ 'sender_street' ]; ?> <?php echo $data[ 'sender_street_no' ]?: $data[ 'sender_street_nr' ]; ?></div>
+						<div class="sender_street"><?php echo $data[ 'sender_street' ]; ?> <?php echo (isset($data[ 'sender_street_nr' ])) ? $data[ 'sender_street_nr' ] : $data[ 'sender_street_no' ]; ?></div>
 						<div class="sender_city"><?php echo $data[ 'sender_zip_code' ]; ?> <?php echo $data[ 'sender_city' ]; ?></div>
 						<div class="sender_state"><?php echo $data[ 'sender_state' ]; ?></div>
 						<div class="sender_country"><?php echo (isset($data['country'])) ? $data[ 'country' ] : ''; ?></div>
@@ -789,7 +789,7 @@ class WC_Shipcloud_Order
 					<div class="label-shipment-recipient address">
 						<div class="recipient_company"><?php echo (isset($data['recipient_company'])) ? $data[ 'recipient_company' ] : ''; ?></div>
 						<div class="recipient_name"><?php echo $data[ 'recipient_first_name' ]; ?> <?php echo $data[ 'recipient_last_name' ]; ?></div>
-						<div class="recipient_street"><?php echo $data[ 'recipient_street' ]; ?> <?php echo $data[ 'recipient_street_no' ]?: $data[ 'recipient_street_nr' ]; ?></div>
+						<div class="recipient_street"><?php echo $data[ 'recipient_street' ]; ?> <?php echo (isset($data[ 'recipient_street_nr' ])) ? $data[ 'recipient_street_nr' ] : $data[ 'recipient_street_no' ]; ?></div>
 						<div class="recipient_city"><?php echo $data[ 'recipient_zip_code' ]; ?> <?php echo $data[ 'recipient_city' ]; ?></div>
 						<div class="recipient_state"><?php echo (isset($data['recipient_state'])) ? $data[ 'recipient_state' ] : ''; ?></div>
 						<div class="recipient_country"><?php echo $data[ 'recipient_country' ]; ?></div>
