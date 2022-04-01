aced1a01ba726003430fd89af2250a485c682bdd
André Cedik <andre.cedik@gmail.com>
2022-04-01 15:31:17 +0200

    added missing semicolon

--- includes/class-wc-shipping-shipcloud-order.php
+++ includes/class-wc-shipping-shipcloud-order.php
@@ -880,7 +880,7 @@ if ( ! class_exists( 'WC_Shipping_Shipcloud_Order' ) ) {
             }
 			
 			if ( isset( $data['pickup'] ) ) {
-				$pickup = $this->extract_pickup_time( $data, 'create_shipment' )
+				$pickup = $this->extract_pickup_time( $data, 'create_shipment' );
 				if ( ! is_wp_error( $pickup ) && ! empty( $pickup ) ) {
 					$data['pickup'] = $pickup;
 				} else {
