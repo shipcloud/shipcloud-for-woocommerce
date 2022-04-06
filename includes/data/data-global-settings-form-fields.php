7999bc2881edcfd905b7d12bccc9f68042772495
André Cedik <andre.cedik@gmail.com>
2022-04-06 09:13:46 +0200

    latest translation changes

--- includes/data/data-global-settings-form-fields.php
+++ includes/data/data-global-settings-form-fields.php
@@ -45,7 +45,7 @@ return array(
 	'carrier_email' => array(
 		'title'       => __( 'Carrier email', 'shipcloud-for-woocommerce' ),
 		'type'        => 'checkbox',
-		'label'       => __( 'Send notification emails from carriers (supported by DHL and DPD) to recipients on status changes of shipment.', 'shipcloud-for-woocommerce' ),
+		'label'       => __( 'Send notification emails from carriers (supported by DHL, DPD, GLS and Hermes) to recipients on status changes of shipment.', 'shipcloud-for-woocommerce' ),
 		'default'     => 'yes'
 	),
 	
@@ -73,7 +73,7 @@ return array(
 	'calculation' => array(
 		'title'       => __( 'Price calculation', 'shipcloud-for-woocommerce' ),
 		'type'        => 'title',
-		'description' => sprintf( __( 'To get a price for the customers order, you have to setup the price calculation.', 'shipcloud-for-woocommerce' ) )
+		'description' => sprintf( __( 'To get a price for the customer\'s order, you have to set up the price calculation.', 'shipcloud-for-woocommerce' ) )
 	),
 	
 	'disable_calculation' => array(
@@ -94,7 +94,7 @@ return array(
 	'standard_price_products' => array(
 		'title'       => __( 'Standard price products', 'shipcloud-for-woocommerce' ),
 		'type'        => 'price',
-		'description' => __( 'Will be used if no sizes or weight is given to a product or for fallback (have to be entered in EUR).', 'shipcloud-for-woocommerce' ),
+		'description' => __( 'Will be used if no size or weight is given to a product or for fallback (have to be entered in EUR).', 'shipcloud-for-woocommerce' ),
 	),
 	
 	'calculation_type_shipment_classes' => array(
@@ -110,7 +110,7 @@ return array(
 	'standard_price_shipment_classes' => array(
 		'title'       => __( 'Standard price shipment classes', 'shipcloud-for-woocommerce' ),
 		'type'        => 'price',
-		'description' => __( 'Will be used if no sizes or weight is given to a shipping class (have to be entered in EUR).', 'shipcloud-for-woocommerce' ),
+		'description' => __( 'Will be used if no size or weight is given to a shipping class (have to be entered in EUR).', 'shipcloud-for-woocommerce' ),
 	),
 	
 	'banking_information' => array(
@@ -238,19 +238,19 @@ return array(
 	'street_detection' => array(
 		'title'   => __( 'Street detection', 'shipcloud-for-woocommerce' ),
 		'type'    => 'checkbox',
-		'label'   => __( 'Automaticly detect street name and number', 'shipcloud-for-woocommerce' ),
-		'description' => __( 'There are some cases where automatic detection doesn\'t work, due to different naming schemes. Always check the recipients\' address before creating a shipping label!', 'shipcloud-for-woocommerce' ),
+		'label'   => __( 'Automatically detect street name and number', 'shipcloud-for-woocommerce' ),
+		'description' => __( 'There are some cases where automatic detection doesn\'t work, due to different naming schemes. Always check the recipient\'s address before creating a shipping label!', 'shipcloud-for-woocommerce' ),
 		'default' => 'yes'
 	),
-	
+
 	'webhook_active' => array(
 		'title'   => __( 'Shipment status notification', 'shipcloud-for-woocommerce' ),
 		'type'    => 'checkbox',
-		'label'   => __( 'Enable webhooks to get notified about status changes in your shipments.', 'shipcloud-for-woocommerce' ),
-		'description' => sprintf( __( 'If you want to make changes, you can find the settings in your <a href="%s" target="_blank">shipcloud.io webhooks section</a>.', 'shipcloud-for-woocommerce' ), 'https://app.shipcloud.io/de/webhooks' ),
+		'label'   => __( 'Enable webhooks to get notified about status changes of your shipments.', 'shipcloud-for-woocommerce' ),
+		'description' => sprintf( __( 'If you want to make changes, you can find the settings in your <a href="%s" target="_blank">shipcloud account</a>.', 'shipcloud-for-woocommerce' ), 'https://app.shipcloud.io/de/webhooks' ),
 		'default' => 'no'
 	),
-	
+
 	'debug' => array(
 		'title'   => __( 'Debug', 'shipcloud-for-woocommerce' ),
 		'type'    => 'checkbox',
