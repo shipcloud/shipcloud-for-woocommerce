540807ce26385ec232d3ef680caff9b9761f149e
André Cedik <andre.cedik@gmail.com>
2022-04-26 08:23:56 +0200

    split plugin between woocommerce-shipcloud.php and woocommerce-shipping-shipcloud.php

--- woocommerce-shipcloud.php
+++ woocommerce-shipcloud.php
@@ -1,5 +1,20 @@
 <?php
 /**
+ * Plugin URI: https://www.wordpress.org/plugins/shipcloud-for-woocommerce/
+ * Description: Integrates shipcloud shipment services to your WooCommerce shop.
+ * Version: 2.0.1
+ * Author: shipcloud GmbH
+ * Author URI: https://shipcloud.io
+ * Developer: shipcloud GmbH
+ * Developer URI: https://developers.shipcloud.io
+ * WC requires at least: 5.2
+ * WC tested up to: 6.2.0
+ * Tested up to: 5.9.1
+ * Text Domain: shipcloud-for-woocommerce
+ * Domain Path: /languages/
+ * Copyright: © 2022 shipcloud GmbH
+ * License: GNU General Public License v3.0
+ * License URI: http://www.gnu.org/licenses/gpl-3.0.html
  *
  * @package WC_Shipping_Shipcloud
  */
--- woocommerce-shipping-shipcloud.php
+++ woocommerce-shipping-shipcloud.php
@@ -1,21 +1,6 @@
 <?php
 /**
  * Plugin Name: shipcloud for WooCommerce
- * Plugin URI: https://www.wordpress.org/plugins/shipcloud-for-woocommerce/
- * Description: Integrates shipcloud shipment services to your WooCommerce shop.
- * Version: 2.0.1
- * Author: shipcloud GmbH
- * Author URI: https://shipcloud.io
- * Developer: shipcloud GmbH
- * Developer URI: https://developers.shipcloud.io
- * WC requires at least: 5.2
- * WC tested up to: 6.2.0
- * Tested up to: 5.9.1
- * Text Domain: shipcloud-for-woocommerce
- * Domain Path: /languages/
- * Copyright: © 2022 shipcloud GmbH
- * License: GNU General Public License v3.0
- * License URI: http://www.gnu.org/licenses/gpl-3.0.html
  *
  * This plugin is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
