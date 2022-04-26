61baf856ff2cea62d7d134a658f8d8d2d7d0026c
André Cedik <andre.cedik@gmail.com>
2022-04-26 09:03:51 +0200

    rework header comment

--- woocommerce-shipping-shipcloud.php
+++ woocommerce-shipping-shipcloud.php
@@ -1,6 +1,22 @@
 <?php
 /**
+ * @wordpress-plugin
  * Plugin Name: shipcloud for WooCommerce
+ * Plugin URI: https://www.wordpress.org/plugins/shipcloud-for-woocommerce/
+ * Description: Integrates shipcloud shipment services to your WooCommerce shop.
+ * Version: 2.0.1
+ * Author: shipcloud GmbH
+ * Author URI: https://shipcloud.io
+ * Developer: shipcloud GmbH
+ * Developer URI: https://developers.shipcloud.io
+ * WC requires at least: 5.2
+ * WC tested up to: 6.2.0
+ * Tested up to: 5.9.1
+ * Text Domain: shipcloud-for-woocommerce
+ * Domain Path: /languages/
+ * Copyright: © 2022 shipcloud GmbH
+ * License: GNU General Public License v3.0
+ * License URI: http://www.gnu.org/licenses/gpl-3.0.html
  *
  * This plugin is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -15,6 +31,9 @@
  * You should have received a copy of the GNU General Public License
  * along with this plugin. If not, see License URI.
  * 
+ */
+
+/**
  * @category  Class
  * @package   WC_Shipping_Shipcloud
  * @author   	Daniel Muenter <info@msltns.com>
